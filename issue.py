f5cd23767ee0cc42912d857eea512f6eea2311df
Divyata <simmisharma3455@gmail.com>
2026-04-27 22:05:35 +0530

    Update issue.py

--- issue.py
+++ issue.py
@@ -1,9 +1,9 @@
 from utilis import books , issue_books
 def issued_book():
-    book_id=int(input("enter book id"))
+    book_id=int(input("enter book id : "))
     if book_id in books:
         issue_books[book_id]=books[book_id]
         del books[book_id]
-        print("book issued sucessfull")
+        print("book issued sucessfully")
     else:
         print("book not issued")    
